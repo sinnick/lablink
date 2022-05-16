c8f109a3eb5cede831d2bc2f50df674bd43c73a7
Fernando Masso <fmasso@focasoftware.com>
2022-05-15 22:28:35 -0300

    tested

--- server.js
+++ server.js
@@ -6,33 +6,24 @@ const config = require('./config.json')
 const bodyParser = require('body-parser')
 const axios = require('axios')
 
-
 app.get('/', (req, res) => res.send(`escuchando carpeta, ${config.ruta}`))
 app.listen(port, () => {
     console.log(`Escuchando en puerto ${port}!`)
     console.log(`Origen: ${config.ruta}`)
     console.log(`Destino: ${config.destino}`)
 })
-
 setInterval(() => {
-
     fs.readdir(config.ruta, (err, files) => {
-
         if (err) {
             console.log('no puedo leer directorio', err);
             return;
         }
-
         let fileList = files.filter(file => file.endsWith('.pdf'));
-
         if (fileList.length) {
             console.log('nuevos archivos', { fileList });
             fileList.forEach(file => {
                 try {
-                    //send file on post request
-
                     let filetosend = fs.readFileSync(`${config.ruta}/${file}`, 'base64');
-
                     let request = {
                         method: 'post',
                         url: config.destino,
@@ -44,8 +35,6 @@ setInterval(() => {
                             'Content-Type': 'application/pdf',
                         }
                     }
-                    // console.log({ request });
-                    
                     axios(request)
                     .then(res => {
                         console.log('archivo enviado', res);
@@ -56,24 +45,16 @@ setInterval(() => {
                             err ? console.log('error al renombrar archivo', err) : console.log(`Archivo ${file} movido a carpeta enviados`)
                         })
                     })
-                    
+                    .catch(() => {
+                        console.log('no hay conexion al server de infolab');
+                    })
                 }
                 catch (error) {
                     console.log('no se pudo mover archivo', error);
                 }
-                //stream file to destination
-                // fs.createReadStream(`${config.ruta}/${file}`).pipe(fs.createWriteStream(`${config.destino}/${file}`));
-
-                //
-
             });
-
-
-
         } else {
             console.log('no hay archivos para mover')
         }
-
     })
 }, 5000)
-
